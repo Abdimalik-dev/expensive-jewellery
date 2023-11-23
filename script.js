e20431e2495141344f02e9a98e8050417f09f1d3
Abdimalik-dev <cabdixuseen076@gmail.com>
2023-11-23 21:46:37 +0300

    Commit

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-// Good Luck 💪🏾
+function mostExpensive() {
+    const items = {
+      Diamond_Earrings: 980,
+      Gold_Watch: 250,
+      Pearl_Necklace: 4650,
+    };
+  
+    let maxPrice = 0;
+    let mostExpensiveItem = '';
+  
+    for (const item in items) {
+      if (items[item] > maxPrice) {
+        maxPrice = items[item];
+        mostExpensiveItem = item;
+      }
+    }
+  
+    console.log("The most Expensive One is ", mostExpensiveItem);
+  }
+  
+  mostExpensive();
